e3d017c10a7b278edbeecd5698ce2e9371521baa
He_Ren <1994068337@qq.com>
2023-07-26 23:25:34 +0800

    little update

--- game/localgame.h
+++ game/localgame.h
@@ -121,9 +121,17 @@ public:
 	bool putflag(Cell c)
 	{
 		if(status != 0) return 0;
-		if(shown.get(c) != unknow_cell) return 0;
-		shown.set(c, flag_cell);
-		return 1;
+		if(shown.get(c) == unknow_cell)
+		{
+			shown.set(c, flag_cell);
+			return 1;
+		}
+		if(shown.get(c) == flag_cell)
+		{
+			shown.set(c, unknow_cell);
+			return 1;
+		}
+		return 0;
 	}
 };
 
